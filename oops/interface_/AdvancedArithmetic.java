e792adea99c01fae9a82221fb761d05cafde50f9
Pratyush Mishra <pratyushmishra975@gmail.com>
2024-12-17 10:14:23 +0530

    interface1

---
+++ oops/interface_/AdvancedArithmetic.java
@@ -0,0 +1,20 @@
+package interface_;
+
+public interface AdvancedArithmetic {
+	public abstract int divisorSum(int num);
+}
+
+
+/**
+----
+Create an interface AdvancedArithmetic
+
+which contain method  :
+
+Method name :divisorSum
+return Type    :int
+Argument type:int
+Access Modifier :public
+non-access modifier :abstract
+
+*/
---
+++ oops/interface_/EmployeeTesting.java
@@ -0,0 +1,55 @@
+package interface_;
+
+public class EmployeeTesting {
+
+	public static void main(String[] args) {
+		FullTimeEmployee fe = new FullTimeEmployee(101,"Pratyush",50000,10000);
+		fe.generatePayroll();
+		System.out.println("Total Salary of Employee : "+fe.calculateSalary());
+
+	}
+}
+
+/**
+Employee Payroll Information
+Employee Monthly Salary : 50000.0
+Employee Benefits : 10000.0
+Total Salary of Employee : 60000.0
+*/
+
+/*
+Testing :
+
+Take a main clas  Employee Management System.
+Create instances of  FullTimeEmployee.
+Call the calculateSalary() and generatePayroll() methods to ensure correct calculations and information generation.
+
+Test Case 1 : 
+--------------
+for Negative Salary
+Sample Input:
+if user eneterd negative salary
+
+Expected Output :
+Error: Monthly salary cannot be negative.
+
+
+Test Case 2 :
+--------------
+ Empty Employee Name
+Input:
+
+if employee is passed by the user is ""(empty)
+
+Expected Output:
+
+Error: Employee name cannot be empty.
+
+Test Case 3 :
+--------------
+Sample Input :
+if all the information is valid input
+
+Expected Output :
+Print all the detail information.
+*/
