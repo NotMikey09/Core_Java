531b7cbcdbe06d8056d52e78bf65decc50bd935e
Pratyush Mishra <pratyushmishra975@gmail.com>
2024-12-17 10:23:31 +0530

    interface3

---
+++ oops/interface_/Calculator.java
@@ -0,0 +1,22 @@
+package interface_;
+
+public interface Calculator {
+	public void calculateSum(int ...args);
+}
+
+/**
+-------------
+
+Design a Caluculator Program
+
+Define an interface Calculator :
+ create one abstract method 
+
+Methods :
+
+name 			: calculateSum ()
+Return Type  		:void
+Arguments Type       :varargs type
+Access Modifier 	:public
+
+*/
---
+++ oops/interface_/SimpleCalculator.java
@@ -0,0 +1,21 @@
+package interface_;
+
+public class SimpleCalculator implements Calculator {
+
+	@Override
+	public void calculateSum(int... args) {
+		int sum = 0;
+		for(int i:args) {
+			sum+=i;
+		}
+		System.out.println("Sum of the numbers: "+sum);
+
+	}
+
+}
+
+/*
+ake one normal class SimpleCalculator which implements Calculator interface
+
+in this class just implement the overriden method calculateSum and write the specific logic in this method.
+*/
